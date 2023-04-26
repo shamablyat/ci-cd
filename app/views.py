889bdcc1385fde30f43a9911c8061f3de556d037
tajbaevshamsiddin <tajbaevsh@gmail.com>
2023-04-26 17:09:37 +0500

    a12

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from .models import *
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from serializer import TutorialSerializer
+from .serializer import TutorialSerializer
 from django.http.response import JsonResponse
 from rest_framework.parsers import JSONParser
 # Create your views here.
